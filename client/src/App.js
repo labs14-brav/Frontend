b869080b86686973e360543d88637c9301919c6c
Roger Caballero <rogercp1234@yahoo.com>
2019-07-10 16:37:28 -0700

    completed auth0 signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
+import NavBar from "./components/NavBar";
+
 
 function App() {
   return (
     <div className="App">
+     <NavBar />
       <header className="App-header">Hello Brav!!</header>
+
     </div>
   );
 }
