076c6d91fe78f7ea574da4ee0ce0f11fad3cc009
mjfalahee <mjfalahee@gmail.com>
2019-07-11 09:54:23 -0700

    added axios, and created a local test for data access

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
 import React from "react";
+import axios from 'axios';
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Hello Brav!!</header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    users: []
+  };
+
+  componentDidMount() {
+      axios
+        .get("http://localhost:8080/users")
+        .then(res => {
+          console.log(res.data);
+          this.setState({users: res.data});
+        })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">Hello Brav!!</header>
+        <ul>
+          {this.state.users.map(user => {
+            return <li> {user.username} </li>
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
