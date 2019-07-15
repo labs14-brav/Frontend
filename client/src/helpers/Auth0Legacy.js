9f13c3de24a3527abe78f65226c6a67dc00cfb38
tjforeman <tylerforeman92@gmail.com>
2019-07-15 16:35:26 -0400

    redirect for staging

--- client/src/helpers/Auth0Legacy.js
+++ client/src/helpers/Auth0Legacy.js
@@ -13,7 +13,7 @@ import history from './history';
 const AUTH_CONFIG = {
   "domain": "brav.auth0.com",
   "clientId": "kOeKAq6ue5IChNwFzJwzpwT7oGMzqHGd",
-  "callbackUrl": (process.env.NODE_ENV === 'production') ? "http://www.beabravone.com/home" : "http://localhost:3000/home",
+  "callbackUrl": (process.env.NODE_ENV === 'production') ? "https://brav-staging.netlify.com/home" : "http://localhost:3000/home",
 }
 
 /**
