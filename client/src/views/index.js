deba2e7ba9a4663debbfa23750cfff9a6a34f491
Andy Bettisworth <andy@accreu.com>
2019-07-14 22:50:05 -0500

    Define GenericError view

--- client/src/views/index.js
+++ client/src/views/index.js
@@ -3,6 +3,7 @@
  */
 
 export { default as ErrorBoundary } from './ErrorBoundary';
+export { default as GenericError } from './GenericError';
 export { default as Home } from './Home';
 export { default as Landing } from './Landing';
 export { default as Login } from './Login';
