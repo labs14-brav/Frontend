4fcaa64bcf61c76d75d8154f916b2b97eca7948f
mjfalahee <mjfalahee@gmail.com>
2019-07-17 16:47:10 -0700

    Firebase Auth initialized, and Auth0 removed.

--- client/src/routes/helpers/PrivateRoute.js
+++ client/src/routes/helpers/PrivateRoute.js
@@ -4,7 +4,6 @@
 
 import React, { useState, useEffect, useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { auth0_legacy } from '../../helpers/index';
 import uuid from 'uuid';
 
 /**
@@ -12,10 +11,6 @@ import uuid from 'uuid';
  */
 
 const PrivateRoute = ({ component: Component, errorBoundary: ErrorBoundary, path, exact }) => {
-  if (!auth0_legacy.isAuthenticated()) {
-    auth0_legacy.logout()
-    return <Redirect to="/" />
-  }
 
   if (exact) {
     return <Route key={uuid.v4()} exact path={path} render={props => (
