1469f44b5e5b03731882c58ab888c7eb76ca6ead
mjfalahee <mjfalahee@gmail.com>
2019-07-24 13:55:49 -0700

    Added a Search view

---
+++ client/src/views/Search.js
@@ -0,0 +1,35 @@
+/**
+ * Dependencies
+ */
+
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+/**
+ * Locals
+ */
+
+
+/**
+ * Define view
+ */
+
+function Search() {
+  const [mediators, setUsers] = useState([]);
+  const [currentCase, setCase] = useState({});
+
+  useEffect(() => {
+  }, []);
+
+  return (
+    <div className="App">
+    </div>
+  )
+};
+
+/**
+ * Export view
+ */
+
+export default Search;
