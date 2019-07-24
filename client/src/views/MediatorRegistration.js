0b55d9556114f99c9d6609019c73cc56aae61629
angel-torres <alas4224@gmail.com>
2019-07-24 15:47:31 -0500

    add change handler and submit handler to MediatorRegistration view

--- client/src/views/MediatorRegistration.js
+++ client/src/views/MediatorRegistration.js
@@ -1,43 +1,81 @@
-import React from "react";
-import { 
-    Container, 
-    TextField, 
+import React, { useState } from "react";
+import axios from "axios";
+import {
+    FormGroup,
+    Container,
+    TextField,
     Button,
-    Box 
+    MenuItem,
+    Select,
+    InputLabel,
+    FormControl
 } from "@material-ui/core";
 
-export default function MediatorRegistration() {
-    return (
-        <Container >
+function MediatorRegistration() {
+    const [values, setValues] = useState({
+        license: "",
+        experience: "",
+        specialization: "",
+        language: ""
+    });
 
+    const handleChange = prop => e => {
+        setValues({ ...values, [prop]: e.target.value });
+    };
+
+    const handleSubmit = () => {
+        console.log("values", values);
+    };
+
+    return (
+        <Container maxWidth="sm">
             <h1>Mediator Registration</h1>
-            <Box>
-            <form>
+            <FormGroup>
                 <TextField
-                    name="license"
                     label="License Number"
                     variant="outlined"
-                    />
-                <TextField
-                    name="experience"
-                    label="Years of Experience"
-                    variant="outlined"
-                    />
-                <TextField
-                    name="specialization"
-                    label="Specialization"
-                    variant="outlined"
-                    />
-                <TextField
-                    name="language"
-                    label="Language"
-                    variant="outlined"
-                    />
-                <Button onClick={() => console.log("make post request here")}>
-                    Submit
-                </Button>
-            </form>
-                    </Box>
+                    value={values.license}
+                    onChange={handleChange("license")}
+                />
+                <FormControl>
+                    <InputLabel htmlFor="age-simple">Specialization</InputLabel>
+                    <Select
+                        onChange={handleChange("specialization")}
+                        value={values.specialization}
+                    >
+                        <MenuItem value="accounting">Accounting</MenuItem>
+                        <MenuItem value="automobile">Automobile</MenuItem>
+                        <MenuItem value="landlord & tenant">Landlord & Tenant</MenuItem>
+                    </Select>
+                </FormControl>
+                <FormControl>
+                    <InputLabel htmlFor="age-simple">
+                        Years of Experience
+                    </InputLabel>
+                    <Select
+                        onChange={handleChange("experience")}
+                        value={values.experience}
+                    >
+                        <MenuItem value={`>2yrs`}>{`>2yrs`}</MenuItem>
+                        <MenuItem value={`2-5yrs`}>{`2-5yrs`}</MenuItem>
+                        <MenuItem value={`<5yrs`}>{`<5yrs`}</MenuItem>
+                    </Select>
+                </FormControl>
+                <FormControl>
+                    <InputLabel htmlFor="age-simple">Language</InputLabel>
+                    <Select
+                        onChange={handleChange("language")}
+                        value={values.language}
+                    >
+                        <MenuItem value={"english"}>English</MenuItem>
+                        <MenuItem value={"spanish"}>Spanish</MenuItem>
+                        <MenuItem value={"french"}>French</MenuItem>
+                    </Select>
+                </FormControl>
+                <Button onClick={() => handleSubmit()} >Submit</Button>
+            </FormGroup>
         </Container>
     );
 }
+
+export default MediatorRegistration;
