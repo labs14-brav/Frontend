5aebe4182199b67d0765b0de04c0bfb4fef52586
tjforeman <tylerforeman92@gmail.com>
2019-07-29 14:03:24 -0400

    working on redux implemntation

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore, compose } from 'redux'
 
 /**
  * Render component to DOM
  */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
+);
+
+
+ReactDOM.render(
+<Provider store={store}>
+<App />
+</Provider>
+, document.getElementById('root'));
