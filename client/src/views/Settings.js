8f20d63cc4f60dc7fb4c66f8b906dd24f1bc7b3c
Andy Bettisworth <andy@accreu.com>
2019-07-29 18:19:22 -0500

    Add comments

--- client/src/views/Settings.js
+++ client/src/views/Settings.js
@@ -1,11 +1,25 @@
+/**
+ * Dependencies
+ */
+
 import React from "react";
 import { Link } from 'react-router-dom';
 
-export default function Settings() {
-    return (
-        <div>
-            <h1>User Settings</h1>
-            <Link to="/users/mediator-registration" >Register as Mediator</Link>
-        </div>
-    );
+/**
+ * Define view
+ */
+
+function Settings() {
+  return (
+    <div>
+      <h1>User Settings</h1>
+      <Link to="/users/mediator-registration" >Register as Mediator</Link>
+    </div>
+  );
 }
+
+/**
+ * Export view
+ */
+
+export default Settings
