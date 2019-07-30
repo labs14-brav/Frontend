2a273e00e4cbc5bb1c4cb4c7de14d05187bc4a5a
angel-torres <alas4224@gmail.com>
2019-07-30 11:29:02 -0500

    initial commit

--- client/src/routes/CasesRouter.js
+++ client/src/routes/CasesRouter.js
@@ -2,22 +2,48 @@
  * Dependencies
  */
 
-import React from 'react';
-import { PrivateRoute } from './helpers/index';
-import { Search, CaseShow, ErrorBoundary, CaseForm,UserCaseShow} from '../views/index';
-import { CaseDetails } from '../components/index';
-import uuid from 'uuid';
-
+import React from "react";
+import { PrivateRoute } from "./helpers/index";
+import {
+    Search,
+    CaseShow,
+    ErrorBoundary,
+    CaseForm,
+    UserCaseShow
+} from "../views/index";
+import { CaseDetails } from "../components/index";
+import uuid from "uuid";
 
 /**
  * Define router
  */
 
 const RootRouter = [
-  <PrivateRoute key={uuid.v4()}  exact path="/cases/" component={UserCaseShow} errorBoundary={ErrorBoundary} />,
-  <PrivateRoute key={uuid.v4()} path="/cases/new" component={CaseForm} errorBoundary={ErrorBoundary} />,
-  <PrivateRoute key={uuid.v4()} path="/cases/:id/mediator-search" component={Search} errorBoundary={ErrorBoundary} />,
-  <PrivateRoute key={uuid.v4()} path="/cases/:id" component={CaseDetails} errorBoundary={ErrorBoundary} />
+    <PrivateRoute
+        key={uuid.v4()}
+        exact
+        path="/cases"
+        component={UserCaseShow}
+        errorBoundary={ErrorBoundary}
+    />,
+    <PrivateRoute
+        key={uuid.v4()}
+        path="/cases/new"
+        component={CaseForm}
+        errorBoundary={ErrorBoundary}
+    />,
+    <PrivateRoute
+        key={uuid.v4()}
+        path="/cases/:idmediator-search"
+        component={Search}
+        errorBoundary={ErrorBoundary}
+    />,
+    <PrivateRoute
+        key={uuid.v4()}
+        path="/cases/:id"
+        component={CaseDetails}
+        errorBoundary={ErrorBoundary}
+    />
 ];
 
 /**
