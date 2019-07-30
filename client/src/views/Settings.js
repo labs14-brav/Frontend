e10ed85953ce624387bc60ebaa3565109996bc17
Andy Bettisworth <andy@accreu.com>
2019-07-30 15:08:10 -0500

    Add styling for EditUserLink

--- client/src/views/Settings.js
+++ client/src/views/Settings.js
@@ -48,6 +48,23 @@ const BecomeMediatorLink = styled(Link)({
   }
 })
 
+const EditUserLink = styled(Link)({
+  color: '#333',
+  backgroundColor: '#E5E6E8',
+  boxShadow: '0px 1px 5px 0px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 3px 1px -2px rgba(0,0,0,0.12)',
+  padding: '6px 76px',
+  textDecoration:'none',
+  lineHeight: '1.75',
+  fontWeight: 500,
+  borderRadius: '4px',
+  '&:hover': {
+    backgroundColor: '#bbbbbb',
+  },
+  '&:active': {
+    boxShadow: '0px 5px 5px -3px rgba(0,0,0,0.2), 0px 8px 10px 1px rgba(0,0,0,0.14), 0px 3px 14px 2px rgba(0,0,0,0.12)',
+  }
+})
+
 /**
  * Define view
  */
@@ -93,7 +110,7 @@ function Settings() {
             </Grid>
 
             <Grid item xs={12} md={6}>
-              <Link to="/settings">Edit</Link>
+              <EditUserLink to="/settings">Edit</EditUserLink>
             </Grid>
           </Grid>
 
