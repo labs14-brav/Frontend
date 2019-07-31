9dd7f242a42521f9d27efd23545900419dcea132
mjfalahee <mjfalahee@gmail.com>
2019-07-31 15:51:46 -0700

    Added inputs with dropdowns for filters

--- client/src/components/MediatorFilter.js
+++ client/src/components/MediatorFilter.js
@@ -1,8 +1,46 @@
 import React, { useState, useEffect } from 'react';
+
+/**
+ * Material UI dependencies
+ */
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+
+/**
+ * Material UI Styles
+ */
+
+const useStyles = makeStyles(theme => ({
+    container: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      flexDirection:'column',
+      justifyContent:'center',
+      alignItems:'center',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 200,
+    },
+    dense: {
+      marginTop: 19,
+    },
+    menu: {
+      width: 200,
+    },
+    button: {
+      margin: theme.spacing(1),
+      width: 50,
+    },
+}));
+
+
 
 
 function MediatorFilter(props) {
@@ -10,20 +48,98 @@ function MediatorFilter(props) {
     const [price, setPrice] = useState("");
     const [experience, setExperience] = useState("");
     const [language, setLanguage] = useState("");
+    
+    //material ui styles
+    const classes = useStyles();
 
+    //mimic CDM to populate the inputs
     useEffect(() => {
 
     }, [])
 
-    onChange = event => {
-        
+    //handleChanges for inputs
+    const handleChanges = event => {
+
     }
 
     return (
-        <div className="filter-container">
+        <div className={classes.container}>
+            <form>
+            <div className="left-container">
+                <TextField
+                className={classes.textField}
+                select
+                label="Specialty"
+                value={specialty}
+                helperText="required"
+                onChange={''}
+                SelectProps={{
+                    MenuProps: {
+                    className: classes.menu,
+                    }
+                }}
+                >
+                    <MenuItem value="Landlord/Tenant">Landlord/Tenant</MenuItem>
+                    <MenuItem value="Eldercare">Eldercare</MenuItem>
+                    <MenuItem value="Commercial">Commercial</MenuItem>
+                    <MenuItem value="Domestic">Domestic</MenuItem>
+                    <MenuItem value="Workplace">Workplace</MenuItem>
+                    <MenuItem value="Penal">Penal</MenuItem>
+                </TextField>
 
+                <TextField
+                className={classes.textField}
+                select
+                label="Price Range"
+                value={price}
+                helperText="required"
+                onChange={''}
+                SelectProps={{
+                    MenuProps: {
+                    className: classes.menu,
+                    }
+                }}
+                >
+                    <MenuItem value="Landlord/Tenant">Landlord/Tenant</MenuItem>
+                    <MenuItem value="Penal">Penal</MenuItem>
+                </TextField>
+            </div>
+            <div className="right-container">
+                <TextField
+                    className={classes.textField}
+                    select
+                    label="Language"
+                    value={language}
+                    helperText="required"
+                    onChange={''}
+                    SelectProps={{
+                        MenuProps: {
+                        className: classes.menu,
+                        }
+                    }}
+                    >
+                        <MenuItem value="Landlord/Tenant">Landlord/Tenant</MenuItem>
+                        <MenuItem value="Penal">Penal</MenuItem>
+                </TextField>
 
-
+                <TextField
+                    className={classes.textField}
+                    select
+                    label="Experience"
+                    value={experience}
+                    helperText="required"
+                    onChange={''}
+                    SelectProps={{
+                        MenuProps: {
+                        className: classes.menu,
+                        }
+                    }}
+                    >
+                        <MenuItem value="Landlord/Tenant">Landlord/Tenant</MenuItem>
+                        <MenuItem value="Penal">Penal</MenuItem>
+                </TextField>
+            </div>
+            </form>
         </div>
     )
 
