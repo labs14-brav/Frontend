c197b2893b552c2c061052cf8fa3fe83b588539a
mjfalahee <mjfalahee@gmail.com>
2019-07-31 15:24:52 -0700

    Setting up the skeleton for MediatorFilter component

--- client/src/components/MediatorFilter.js
+++ client/src/components/MediatorFilter.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 
 
 function MediatorFilter(props) {
+    const [specialty, setSpecialty] = useState("");
+    const [price, setPrice] = useState("");
+    const [experience, setExperience] = useState("");
+    const [language, setLanguage] = useState("");
+
+    useEffect(() => {
+
+    }, [])
+
+    onChange = event => {
+        
+    }
+
+    return (
+        <div className="filter-container">
+
+
+
+        </div>
+    )
 
 }
 
