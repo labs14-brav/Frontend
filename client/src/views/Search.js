35eabd864dc52cce19c6608e444e764ee47adab9
mjfalahee <mjfalahee@gmail.com>
2019-07-31 15:14:29 -0700

    Imported MediatorFilter into Search view

--- client/src/views/Search.js
+++ client/src/views/Search.js
@@ -4,7 +4,7 @@
 
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import MediatorList from '../components/MediatorList';
+import { MediatorList, MediatorFilter } from '../components/index';
 
 
 /**
