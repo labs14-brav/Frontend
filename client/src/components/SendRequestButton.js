d4b256a5b4eae09f7763ef8166d43fe03ec7d742
Brennon Nuckols <brennuck@gmail.com>
2019-08-05 12:17:57 -0600

    Send request button

---
+++ client/src/components/SendRequestButton.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+ function SendRequestButton(props) {
+    function handleRequest() {
+        axios.post(`/mediators/${props.mediator.id}/cases`)
+    }
+
+     return (
+        <SendRequestButton onClick={handleRequest}>
+            Send Request
+        </SendRequestButton>
+    )
+}
+
+ export default SendRequestButton;
