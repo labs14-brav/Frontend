ef350f9635af750670deafd3f7fda748a1c20122
mjfalahee <mjfalahee@gmail.com>
2019-08-06 14:31:25 -0700

    Quick fix

--- client/src/components/modals/SimpleDialog.js
+++ client/src/components/modals/SimpleDialog.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/button';
+import Button from '@material-ui/core/Button';
 
 import { withRouter } from 'react-router-dom';
 
