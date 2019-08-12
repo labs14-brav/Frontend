31cc468477b70f2be66a5828ff67057a9039d950
Roger Caballero <rogercp1234@yahoo.com>
2019-08-12 15:46:01 -0700

    added state for pages

--- client/src/components/MediatorList.js
+++ client/src/components/MediatorList.js
@@ -13,9 +13,16 @@ import Grid from '@material-ui/core/Grid';
 
 const MediatorList = (props) => {
     const [mediators, setMediators] = useState([]);
+    const [loading, setLoading]=useState(false);
+    const [currentPage,setCurrentPage]=useState(1);
+    const [mediatorsPerPage, setMediatorsPerPage]=useState(2);
+
+
+
 
     useEffect(() => {
         async function fetchMediators() {
+            setLoading(true);
             const res = await axioswithAuth().get(`${process.env.REACT_APP_API_URL}/mediators`, {
                 params: {
                     language: props.filter.language,
@@ -25,15 +32,21 @@ const MediatorList = (props) => {
                 }
             });
             setMediators(res.data);
+            setLoading(false);
         }
         fetchMediators()
     }, [props.filter]);
 
+
+    const indexOfLastMediator=currentPage*mediatorsPerPage;
+    const indexOfFirstMediatior=indexOfLastMediator-mediatorsPerPage;
+    const currentMediators=mediators.slice(indexOfFirstMediatior,indexOfLastMediator);
+
     return (
         <Grid container spacing={4} justify="space-evenly">
-            {mediators.map(mediator => {
+            {currentMediators.map(mediator => {
                 return (
-                    <MediatorCard mediator={mediator}  numMediators={mediator.length} currentcase={props.currentcase} key={mediator.uid} />
+                    <MediatorCard mediator={currentMediators}  numMediators={mediator.length} currentcase={props.currentcase} key={mediator.uid} />
                  );
             })}
         </Grid>
