72f5e4c7054a0f0e218374553342e8ae2cd5205c
Andy Bettisworth <andy@accreu.com>
2019-08-13 14:52:57 -0500

    Remove comments

--- client/src/views/Landing.test.js
+++ client/src/views/Landing.test.js
@@ -26,9 +26,6 @@ function renderWithRouter(
 ) {
   return {
     ...render(<Router history={history}>{ui}</Router>),
-    // adding `history` to the returned utilities to allow us
-    // to reference it in our tests (just try to avoid using
-    // this to test implementation details).
     history,
   }
 }
