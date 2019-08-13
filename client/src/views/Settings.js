142eb38919e4796cc3f9f0b47925ef87b9b92293
angel-torres <alas4224@gmail.com>
2019-08-13 11:42:23 -0500

    updated information on settings cards; also added conditional render for becoming a mediator card on settings so that those who are already mediators aren't prompted to apply again

--- client/src/views/Settings.js
+++ client/src/views/Settings.js
@@ -36,7 +36,7 @@ const useStyles = makeStyles(() => ({
         flexDirection: "column",
         justifyContent: "space-between",
         alignItems: "center",
-        minHeight: "150px",
+        minHeight: "200px",
         margin: "10px",
         padding: "30px"
     },
@@ -58,11 +58,18 @@ const useStyles = makeStyles(() => ({
     },
     cardContent: {
         display: "flex",
+        flexDirection: "column",
         justifyContent: "space-between",
         width: "100%",
-        paddingTop: "30px",
-        flexDirection: "column",
-        height: "120px"
+        margin: "30px 0px",
+        color: "grey",
+        minHeight: "80px"
+    },
+    text: {
+        margin: "20px 0px"
+    },
+    link: {
+        textDecoration: "none"
     }
 }));
 
@@ -72,37 +79,51 @@ const useStyles = makeStyles(() => ({
 
 function Settings() {
     const classes = useStyles();
+    const userType = localStorage.getItem("type");
+    console.log(userType);
 
     return (
         <div className={classes.container}>
             <h3 className={classes.title}>Site settings</h3>
             <p>General user settings</p>
             <section className={classes.cardContainer}>
-                <Card className={classes.card}>
-                    <div className={classes.cardTitle}>
-                        <strong>Become a Brav one!</strong>
-                        <div className={classes.divider}> </div>
-                    </div>
-                    <div className={classes.cardContent}>
-                        <p>
-                            Become a Brav one! Click the button below to apply!
-                        </p>
-                        <Link to="/users/mediator-registration">
-                            <BecomeMediatorLink>
-                                Become a Mediator
-                            </BecomeMediatorLink>
-                        </Link>
-                    </div>
-                </Card>
+                {userType === "mediator" ? null : (
+                    <Card className={classes.card}>
+                        <div className={classes.cardTitle}>
+                            <strong>Become mediator</strong>
+                            <div className={classes.divider}> </div>
+                        </div>
+                        <div className={classes.cardContent}>
+                            <p className={classes.text}>
+                                Are you a certified mediator? Join the Brav
+                                platform to offer your services! Upon approval
+                                you will be able to receive mediation requests
+                                and get payed right from the app. Click below to
+                                submit your application!
+                            </p>
+                            <Link
+                                className={classes.link}
+                                to="/users/mediator-registration"
+                            >
+                                <BecomeMediatorLink>
+                                    Become a Mediator
+                                </BecomeMediatorLink>
+                            </Link>
+                        </div>
+                    </Card>
+                )}
+
                 <Card className={classes.card}>
                     <div className={classes.cardTitle}>
                         <strong>Deactivate account</strong>
                         <div className={classes.divider}> </div>
                     </div>
                     <div className={classes.cardContent}>
-                        <p>
-                            Click below to deactivate account. Action is
-                            irreversible.
+                        <p className={classes.text}>
+                            If you deactivate your account you will lose access
+                            to our services. A record of your cases will be
+                            stored as stated in the agreement. Click below if
+                            you are sure to continue.
                         </p>
                         <DeactivateAccountButton />
                     </div>
--- client/src/views/styles/BecomeMediatorLink.js
+++ client/src/views/styles/BecomeMediatorLink.js
@@ -15,7 +15,6 @@ const BecomeMediatorLink = styled(Button)({
     boxShadow:
         "0px 1px 5px 0px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 3px 1px -2px rgba(0,0,0,0.12)",
     padding: "6px 16px",
-    textDecoration: "none",
     lineHeight: "1.75",
     fontWeight: 500,
     borderRadius: "4px",
@@ -25,7 +24,8 @@ const BecomeMediatorLink = styled(Button)({
     "&:active": {
         boxShadow:
             "0px 5px 5px -3px rgba(0,0,0,0.2), 0px 8px 10px 1px rgba(0,0,0,0.14), 0px 3px 14px 2px rgba(0,0,0,0.12)"
-    }
+    },
+    textDecoration: "none"
 });
 
 /**
