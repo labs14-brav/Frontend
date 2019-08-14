3171bacc2aef54d8430d496d32a1d2927c3cb7aa
Andy Bettisworth <andy@accreu.com>
2019-08-14 12:25:12 -0500

    Add tests for OutsideCourtForm view

---
+++ client/src/views/OutsideCourtForm.test.js
@@ -0,0 +1,60 @@
+/**
+ * Dependencies
+ */
+
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+import { Router } from 'react-router-dom';
+import { cleanup, render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import OutsideCourtForm from './OutsideCourtForm';
+
+/**
+ * Hooks
+ *   cleanup - Unmounts React trees that were mounted with render.
+ */
+
+afterEach(cleanup);
+
+/**
+ * Helpers
+ */
+
+function renderWithRouter(
+  ui,
+  {route = '/', history = createMemoryHistory({initialEntries: [route]})} = {},
+) {
+  return {
+    ...render(<Router history={history}>{ui}</Router>),
+    history,
+  }
+}
+
+/**
+ * Assertions
+ */
+
+describe('OutsideCourtForm.js', () => {
+  test('it renders without errors', () => {
+    const route = '/cases/new/outside'
+    renderWithRouter(<OutsideCourtForm />, {route})
+  });
+
+  test('it has a heading', () => {
+    const route = '/cases/new/outside'
+    const { getByTestId } = renderWithRouter(<OutsideCourtForm />, {route})
+
+    expect(getByTestId('heading-h3').textContent).toBe('Case Form')
+  });
+
+  test('it has required form inputs', () => {
+    const route = '/cases/new/outside'
+    const { getByTestId } = renderWithRouter(<OutsideCourtForm />, {route})
+
+    expect(getByTestId('input-dispute_category').textContent).toBe('Dispute Type​​required')
+    expect(getByTestId('input-parties_involved').textContent).toBe('Dispute Participants​')
+    expect(getByTestId('input-parties_contact_info').textContent).toBe('Participant Contact Info​')
+    expect(getByTestId('input-dispute_amount').textContent).toBe('Dispute Amount - $​if applicable')
+    expect(getByTestId('input-description').textContent).toBe('Description of Conflict​')
+  });
+});
