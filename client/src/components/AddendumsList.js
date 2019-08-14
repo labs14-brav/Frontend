352ed333290ce2f58c32548a1d3d7db4588a72cd
Andy Bettisworth <andy@accreu.com>
2019-08-14 11:45:59 -0500

    Add tests for UserCaseShow view

--- client/src/components/AddendumsList.js
+++ client/src/components/AddendumsList.js
@@ -1,39 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import { axioswithAuth } from '../helpers/index';
+/**
+ * Dependencies
+ */
 
+import React, { useState, useEffect } from 'react';
+import axioswithAuth from '../helpers/axioswithAuth';
 import Addendum from './Addendum';
+
+/**
+ * Import styles
+ */
+
 import './Addendum.scss';
 
+/**
+ * Define component
+ */
+
 function AddendumsList(props) {
-    const [addendums, setAddendums] = useState([]);
+  const [addendums, setAddendums] = useState([]);
 
-    useEffect(() => {
-        axioswithAuth().get(`${process.env.REACT_APP_API_URL}/cases/${props.case.id}/addendums`)
-        .then(res => {
-            setAddendums(res.data);
-        }) 
-        .catch (err => {
-            console.error(err);
-        })
-    }, [])
+  useEffect(() => {
+    axioswithAuth().get(`${process.env.REACT_APP_API_URL}/cases/${props.case.id}/addendums`)
+      .then(res => {
+        setAddendums(res.data);
+      })
+      .catch (err => {
+        console.error(err);
+      })
+  }, [])
 
-        if (addendums.length === 0) {
-            return(
-                <>
-                <h3 id="addendums-blank"> This case has no additional details to view. </h3>
-                </>
-            )
-        } else {
-            return ( 
-                <>
-                <ul id="addendums-list">
-                {addendums.map(adden => {
-                    return <li><Addendum text={adden.description} timestamp={adden.created_at}/></li>
-                })}
-             </ul>
-             </>
-             )
-        }
+  if (addendums.length === 0) {
+    return <h3 id="addendums-blank">This case has no additional details to view.</h3>
+  } else {
+    return (
+      <ul id="addendums-list">
+        {addendums.map(addendum => {
+          return <li><Addendum text={addendum.description} timestamp={addendum.created_at}/></li>
+        })}
+      </ul>
+    )
+  }
 }
+
+/**
+ * Export component
+ */
 
 export default AddendumsList;
--- client/src/views/UserCaseShow.js
+++ client/src/views/UserCaseShow.js
@@ -4,8 +4,8 @@
 
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { UserCaseList } from '../components/index';
 import Button from '@material-ui/core/Button';
+import UserCaseList from '../components/UserCaseList';
 
 /**
  * Import styles
