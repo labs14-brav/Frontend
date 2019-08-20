0af840ce0d93996e3eb754f3096e5a88e8a1712c
Andy Bettisworth <andy@accreu.com>
2019-08-20 09:08:22 -0500

    Test user can access Case Form

--- client/cypress/integration/examples/create-case.spec.js
+++ client/cypress/integration/examples/create-case.spec.js
@@ -1,11 +1,15 @@
 /// <reference types="Cypress" />
 
-context('User Login', () => {
+context('Create Case', () => {
   beforeEach(() => {
     cy.visit('https://brav-staging.netlify.com')
   })
 
-  it('has a login form', () => {
+  it('has a case form', () => {
+    /**
+     * User Login
+     */
+
     cy.get('[data-testid="login-button"]').click()
     cy.contains('Sign in with email').click()
     cy.get('input[name="email"]').type('labs14brav-user@gmail.com')
@@ -14,5 +18,15 @@ context('User Login', () => {
     cy.contains('Sign In').click()
     cy.wait(1000)
     cy.get('.bravHeader').contains("Brāv")
+
+    /**
+     * View Form
+     */
+
+    cy.contains('Create a Case').click()
+    cy.contains('button', 'Non-Court Referral')
+    cy.contains('button', 'Court Referral').click()
+    cy.wait(1000)
+    cy.get('[data-testid="heading-h3"]').contains("Case Form")
   })
 })
