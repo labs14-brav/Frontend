ae246141e5f5654e0e65fad35b80db831d398b59
Andy Bettisworth <andy@accreu.com>
2019-08-22 11:11:43 -0500

    Indent to make reading path in Router easier

--- client/src/routes/CasesRouter.js
+++ client/src/routes/CasesRouter.js
@@ -20,46 +20,30 @@ import {
  */
 
 const RootRouter = [
-    <PrivateRoute
-        key={uuid.v4()}
-        exact
-        path='/cases'
+    <PrivateRoute key={uuid.v4()} exact path='/cases'
         component={UserCaseShow}
         errorBoundary={ErrorBoundary}
     />,
-    <PrivateRoute
-        key={uuid.v4()}
-        exact
-        path='/cases/new'
+    <PrivateRoute key={uuid.v4()} exact path='/cases/new'
         component={NewCase}
         errorBoundary={ErrorBoundary}
     />,
-    <PrivateRoute
-        key={uuid.v4()}
-        path='/cases/new/court'
+    <PrivateRoute key={uuid.v4()} path='/cases/new/court'
         component={CourtForm}
         errorBoundary={ErrorBoundary}
     />,
-    <PrivateRoute
-        key={uuid.v4()}
-        path='/cases/new/outside'
+    <PrivateRoute key={uuid.v4()} path='/cases/new/outside'
         component={OutsideCourtForm}
         errorBoundary={ErrorBoundary}
     />,
-    <PrivateRoute
-        key={uuid.v4()}
-        exact
-        path='/mediator-cases'
+    <PrivateRoute key={uuid.v4()} exact path='/mediator-cases'
         component={MediatorCasesShow}
         errorBoundary={ErrorBoundary}
     />,
-    <PrivateRoute
-        key={uuid.v4()}
-        path='/cases/:id/mediator-search'
+    <PrivateRoute key={uuid.v4()} path='/cases/:id/mediator-search'
         component={Search}
         errorBoundary={ErrorBoundary}
     />,
-
 ];
 
 /**
