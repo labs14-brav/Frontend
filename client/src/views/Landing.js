c1c47769073eeb65789c69c1f8d37026f028f818
Roger Caballero <rogercp1234@yahoo.com>
2019-08-30 10:06:04 -0700

    changed the word solve to manage

--- client/src/views/Landing.js
+++ client/src/views/Landing.js
@@ -246,7 +246,7 @@ function Landing() {
         <div className="how">
         <h3>How Brāv Works</h3>
         <div className="alternativeMediation">
-             <h6>Mediation is a great alternative to the formal judicial system. Our platform makes mediation even easier for users to solve conflict through the use of our online tools.</h6>
+             <h6>Mediation is a great alternative to the formal judicial system. Our platform makes mediation even easier for users to manage conflict through the use of our online tools.</h6>
         </div>
             <div className="howContent">
 
