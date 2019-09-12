f9a0dc38910886c2e6100126ded00e72e0de4974
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-09-12 18:11:27 -0400

    Failed logins will throw an error.

--- client/src/views/AuthCallback.js
+++ client/src/views/AuthCallback.js
@@ -12,57 +12,56 @@ import { mixpanel } from "../helpers/index";
  */
 
 function AuthCallback(props) {
-    firebase.auth().onAuthStateChanged(async user => {
-        // User is signed in.
-        if (user) {
-            let token = await user.getIdToken();
-            localStorage.setItem("token", token);
+  firebase.auth().onAuthStateChanged(async user => {
+    // User is signed in.
+    if (user) {
+      let token = await user.getIdToken();
+      localStorage.setItem("token", token);
 
-            axios
-                .post(`${process.env.REACT_APP_API_URL}/users/auth`, {
-                    user: user,
-                    token: token
-                })
-                .then(res => {
-                    localStorage.setItem("type", res.data.type);
-                    localStorage.setItem("id", res.data.id);
-                    if (res.data.type === "mediator") {
-                        if (process.env.NODE_ENV === "production") {
-                            mixpanel.track("Mediator sign in", {
-                                distinct_id: localStorage.getItem("id")
-                            });
-                        }
+      axios
+        .post(`${process.env.REACT_APP_API_URL}/users/auth`, {
+          user: user,
+          token: token
+        })
+        .then(res => {
+          localStorage.setItem("type", res.data.type);
+          localStorage.setItem("id", res.data.id);
+          if (res.data.type === "mediator") {
+            if (process.env.NODE_ENV === "production") {
+              mixpanel.track("Mediator sign in", {
+                distinct_id: localStorage.getItem("id")
+              });
+            }
 
-                        window.location = "/mediator-cases";
-                    } else if (res.data.type === "admin") {
-                        if (process.env.NODE_ENV === "production") {
-                            mixpanel.track("Admin sign in", {
-                                distinct_id: localStorage.getItem("id")
-                            });
-                        }
+            window.location = "/mediator-cases";
+          } else if (res.data.type === "admin") {
+            if (process.env.NODE_ENV === "production") {
+              mixpanel.track("Admin sign in", {
+                distinct_id: localStorage.getItem("id")
+              });
+            }
 
-                        window.location = "/admin";
-                    } else {
-                        if (process.env.NODE_ENV === "production") {
-                            mixpanel.track("User sign in", {
-                                distinct_id: localStorage.getItem("id")
-                            });
-                        }
+            window.location = "/admin";
+          } else {
+            if (process.env.NODE_ENV === "production") {
+              mixpanel.track("User sign in", {
+                distinct_id: localStorage.getItem("id")
+              });
+            }
 
-                        window.location = "/cases";
-                    }
-                })
-                .catch(err => {
-                    console.error(err);
-                    window.location = "/users/login";
-                });
-        } else {
-            // User is signed out.
-            window.location = "/users/login";
-        }
-    });
+            window.location = "/cases";
+          }
+        })
+        .catch(err => {
+          throw new Error("Sign in failed");
+        });
+    } else {
+      // User is signed out.
+      window.location = "/users/login";
+    }
+  });
 
-    return <div className="login" />;
+  return <div className="login" />;
 }
 
 /**
