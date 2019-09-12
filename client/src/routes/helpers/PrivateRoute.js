57567d1713a2abfc5749e2ba6eef048b6c6bbb9f
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-09-12 18:03:32 -0400

    Moved authentication login outside of PrivateRoute loading.

--- client/src/routes/helpers/PrivateRoute.js
+++ client/src/routes/helpers/PrivateRoute.js
@@ -2,60 +2,45 @@
  * Dependencies
  */
 
-import React from 'react';
-import { Route } from 'react-router-dom';
-import firebase from 'firebase';
-import uuid from 'uuid';
-import axios from 'axios';
+import React from "react";
+import { Route } from "react-router-dom";
+import uuid from "uuid";
 
 /**
  * Define route component
  */
 
-const PrivateRoute = ({ component: Component, errorBoundary: ErrorBoundary, path, exact }) => {
-  firebase.auth().onAuthStateChanged(async (user) => {
-    // User is signed in.
-    if (user) {
-      let displayName = user.displayName;
-      let email = user.email;
-      let emailVerified = user.emailVerified;
-      let photoURL = user.photoURL;
-      let isAnonymous = user.isAnonymous;
-      let uid = user.uid;
-      let providerData = user.providerData;
-
-      let token = await user.getIdToken();
-
-      localStorage.setItem('token', token);
-
-      axios.post(`${process.env.REACT_APP_API_URL}/users/auth`, {
-        user: user,
-        token: token
-      }).then(res => {
-        localStorage.setItem('type', res.data.type);
-        localStorage.setItem('id', res.data.id);
-        localStorage.setItem('is_stripe_connected', res.data.is_stripe_connected);
-      }).catch(err => {
-        console.error(err)
-      })
-    } else {
-      // User is signed out.
-      // ...
-    }
-  });
-
+const PrivateRoute = ({
+  component: Component,
+  errorBoundary: ErrorBoundary,
+  path,
+  exact
+}) => {
   if (exact) {
-    return <Route key={uuid.v4()} exact path={path} render={props => (
-      <ErrorBoundary>
-        <Component {...props} />
-      </ErrorBoundary>
-    )} />
+    return (
+      <Route
+        key={uuid.v4()}
+        exact
+        path={path}
+        render={props => (
+          <ErrorBoundary>
+            <Component {...props} />
+          </ErrorBoundary>
+        )}
+      />
+    );
   } else {
-    return <Route key={uuid.v4()} path={path} render={props => (
-      <ErrorBoundary>
-        <Component {...props} />
-      </ErrorBoundary>
-    )} />
+    return (
+      <Route
+        key={uuid.v4()}
+        path={path}
+        render={props => (
+          <ErrorBoundary>
+            <Component {...props} />
+          </ErrorBoundary>
+        )}
+      />
+    );
   }
 };
 
