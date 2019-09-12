abe2b834e66c0e68f221b2237f92961b77e887ec
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-09-12 17:01:25 -0400

    Fixed bug where attempting to login without refreshing the page would crash the app."

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -2,11 +2,11 @@
  * Dependencies
  */
 
-import React, { useState, useEffect } from 'react';
-import Card from '@material-ui/core/Card';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import { makeStyles } from '@material-ui/core/styles';
-import { firebase } from '../helpers/index';
+import React, { useState, useEffect } from "react";
+import Card from "@material-ui/core/Card";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import { makeStyles } from "@material-ui/core/styles";
+import { firebase } from "../helpers/index";
 
 /**
  * Define styles
@@ -14,9 +14,9 @@ import { firebase } from '../helpers/index';
 
 const useStyles = makeStyles(() => ({
   card: {
-    paddingTop: "50px",
+    paddingTop: "50px"
   }
-}))
+}));
 
 /**
  * Define view
@@ -24,24 +24,27 @@ const useStyles = makeStyles(() => ({
 
 function Login(props) {
   const classes = useStyles();
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    setIsLoading(false);
+    // setIsLoading(false);
     firebase();
   }, []);
 
-  if (isLoading) return <LinearProgress />
+  // if (isLoading) return <LinearProgress />;
 
   return (
     <div className="login">
-      <Card className={classes.card} style={{  backgroundColor:"rgb(212, 212, 211)",height:"100vh"}}>
-        <div id='firebaseui-auth-container'></div>
-        <div id='loader'></div>
+      <Card
+        className={classes.card}
+        style={{ backgroundColor: "rgb(212, 212, 211)", height: "100vh" }}
+      >
+        <div id="firebaseui-auth-container"></div>
+        <div id="loader"></div>
       </Card>
     </div>
-  )
-};
+  );
+}
 
 /**
  * Export view
