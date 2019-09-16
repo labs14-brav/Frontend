d38a5568d383418e67f26eef3a405ebb8733bd49
angel-torres <alas4224@gmail.com>
2019-09-16 16:47:56 -0500

    changed the words in the navbar to icons from font awesome

---
+++ client/src/components/styles/_navbar.js
@@ -0,0 +1,15 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+    navbar: {
+        backgroundColor: "#375C80",
+        // height: "80px"
+    },
+    icon: {
+        color: "white",
+        height: "50px",
+    }
+}));
+
+export default useStyles;
+
