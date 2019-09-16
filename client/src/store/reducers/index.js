d75fea65d86bd04d3816e87d6dde4c65ff318fd1
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-09-16 18:35:56 -0400

    Added authReducer to combineReducers.

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,11 +1,12 @@
-import {combineReducers} from 'redux';
-import {fetchUsersReducer} from './fetchUsersReducer'
-import {mediatorRequestReducer} from './mediatorRequestReducer'
-import {fetchMediatorsReducer} from './fetchMediatorsReducer'
-
+import { combineReducers } from "redux";
+import { fetchUsersReducer } from "./fetchUsersReducer";
+import { mediatorRequestReducer } from "./mediatorRequestReducer";
+import { fetchMediatorsReducer } from "./fetchMediatorsReducer";
+import { authReducer } from "./AuthReducer";
 
 export default combineReducers({
-fetchUsersReducer,
-mediatorRequestReducer,
-fetchMediatorsReducer
-})
+  fetchUsersReducer,
+  mediatorRequestReducer,
+  fetchMediatorsReducer,
+  authReducer
+});
