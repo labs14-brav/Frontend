fc73853b3103d0ce07c4344b8834ef5bfde95ab1
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-09-16 18:21:21 -0400

    Moved axios call for user login to web backend to its own action creator, with the dispatch flow any action creator making API calls.

--- client/src/store/actions/Authenticate.js
+++ client/src/store/actions/Authenticate.js
@@ -1,3 +1,46 @@
 import { LOG_IN_START, LOG_IN_SUCCESS, LOG_IN_FAILURE } from "./index";
+import axios from "axios";
+import { mixpanel } from "../helpers/index";
 
-export const authenticateUser = () => {};
+const URL = process.env.REACT_APP_API_URL;
+const environment = process.env.NODE_ENV;
+
+export const authenticateUser = requestData => dispatch => {
+  dispatch({ type: LOG_IN_START });
+  axios
+    .post(`${URL}/users/auth`, requestData)
+    .then(res => {
+      localStorage.setItem("type", res.data.type);
+      localStorage.setItem("id", res.data.id);
+      dispatch({ type: LOG_IN_SUCCESS });
+      if (res.data.type === "mediator") {
+        if (environment === "production") {
+          mixpanel.track("Mediator sign in", {
+            distinct_id: localStorage.getItem("id")
+          });
+        }
+
+        window.location = "/mediator-cases";
+      } else if (res.data.type === "admin") {
+        if (environment === "production") {
+          mixpanel.track("Admin sign in", {
+            distinct_id: localStorage.getItem("id")
+          });
+        }
+
+        window.location = "/admin";
+      } else {
+        if (environment === "production") {
+          mixpanel.track("User sign in", {
+            distinct_id: localStorage.getItem("id")
+          });
+        }
+
+        window.location = "/cases";
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      dispatch({ type: LOG_IN_FAILURE });
+    });
+};
--- client/src/views/AuthCallback.js
+++ client/src/views/AuthCallback.js
@@ -4,8 +4,6 @@
 
 import React from "react";
 import firebase from "firebase";
-import axios from "axios";
-import { mixpanel } from "../helpers/index";
 
 /**
  * Define view
@@ -17,44 +15,8 @@ function AuthCallback(props) {
     if (user) {
       let token = await user.getIdToken();
       localStorage.setItem("token", token);
-
-      axios
-        .post(`${process.env.REACT_APP_API_URL}/users/auth`, {
-          user: user,
-          token: token
-        })
-        .then(res => {
-          localStorage.setItem("type", res.data.type);
-          localStorage.setItem("id", res.data.id);
-          if (res.data.type === "mediator") {
-            if (process.env.NODE_ENV === "production") {
-              mixpanel.track("Mediator sign in", {
-                distinct_id: localStorage.getItem("id")
-              });
-            }
-
-            window.location = "/mediator-cases";
-          } else if (res.data.type === "admin") {
-            if (process.env.NODE_ENV === "production") {
-              mixpanel.track("Admin sign in", {
-                distinct_id: localStorage.getItem("id")
-              });
-            }
-
-            window.location = "/admin";
-          } else {
-            if (process.env.NODE_ENV === "production") {
-              mixpanel.track("User sign in", {
-                distinct_id: localStorage.getItem("id")
-              });
-            }
-
-            window.location = "/cases";
-          }
-        })
-        .catch(err => {
-          throw new Error("Sign in failed");
-        });
+      // call log in action creator here
+      // pass in an object with user and token
     } else {
       // User is signed out.
       window.location = "/users/login";
