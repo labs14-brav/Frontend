c80c950d5c15472cf6c9d399da185bc7e2a7884e
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-09-18 18:35:18 -0400

    Removed unnecessary in-line styles for Login, as well as useStyles MUI custom hook.

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -2,39 +2,19 @@
  * Dependencies
  */
 
-import React, { useState, useEffect } from "react";
-import Card from "@material-ui/core/Card";
+import React, { useEffect } from "react";
 import Dialog from "@material-ui/core/Dialog";
-import LinearProgress from "@material-ui/core/LinearProgress";
-import { makeStyles } from "@material-ui/core/styles";
 import { firebase } from "../helpers/index";
-
-/**
- * Define styles
- */
-
-const useStyles = makeStyles(() => ({
-  card: {
-    paddingTop: "50px"
-  }
-}));
 
 /**
  * Define view
  */
 
 function Login({ loginDialog, closeLogin, test, setTest }) {
-  const classes = useStyles();
-  // const [isLoading, setIsLoading] = useState(true)
-
   useEffect(() => {
-    // setIsLoading(false);
     if (loginDialog) {
       // console.log("effect fired");
       setTest(true);
-      // setTimeout(() => {
-      //   firebase();
-      // }, 500);
     }
   }, [loginDialog]);
   useEffect(() => {
@@ -45,15 +25,8 @@ function Login({ loginDialog, closeLogin, test, setTest }) {
     }
   }, [test]);
 
-  // if (isLoading) return <LinearProgress />;
-
   return (
-    <Dialog
-      open={loginDialog}
-      className={classes.card}
-      onClose={closeLogin}
-      style={{ backgroundColor: "rgb(212, 212, 211)", height: "100vh" }}
-    >
+    <Dialog open={loginDialog} onClose={closeLogin}>
       <div className="login">
         <div id="firebaseui-auth-container"></div>
         <div id="loader"></div>
